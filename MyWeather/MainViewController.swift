78b9dff8d10ed4424ceefcd41c141df222d62153
seongmin kim <k2417000@naver.com>
2024-07-12 21:25:45 +0900

    code base 설정, 스토리보드 삭제

--- MyWeather/MainViewController.swift
+++ MyWeather/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  MyWeather
 //
 //  Created by 김성민 on 7/12/24.
@@ -7,12 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
 }
 
